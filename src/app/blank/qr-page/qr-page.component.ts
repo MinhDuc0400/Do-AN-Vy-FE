a6ab40a28be0baf4e606321c0952d6c4d5b9b722
minhd <minhduc0400@gmail.com>
2023-03-06 23:19:46 +0700

    update module

--- src/app/blank/qr-page/qr-page.component.ts
+++ src/app/blank/qr-page/qr-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from '../../../environments/environment';
 import { NbSpinnerService } from '@nebular/theme';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'ngx-qr-page',
@@ -10,14 +11,29 @@ import { NbSpinnerService } from '@nebular/theme';
 })
 export class QrPageComponent implements OnInit {
   public stringUrl: string;
+  socket: any;
   constructor(
     private route: ActivatedRoute,
     private spinner: NbSpinnerService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
     this.spinner.load();
     this.stringUrl = environment.domainURL + 'blank/' + this.route.snapshot.params.id;
+    const idToken = localStorage.getItem('idToken');
+    this.socket = io(`${environment.socketURL}?token=${idToken}`, {
+      auth: {
+        query: idToken,
+      },
+      transports: ['websocket'],
+      upgrade: false,
+    }).connect();
+    this.socket.on('payment', data => {
+      if (data && data._id && data._id === this.route.snapshot.params.id) {
+        this.router.navigate(['blank']);
+      }
+    });
   }
 
 }
